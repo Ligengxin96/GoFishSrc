a7842bbcbd2af138cb3f8810b4802c3a29f11b8d
Gengxin Li <ligengxin96@gmail.com>
2021-06-04 17:34:56 +0800

    update

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://www.v2ex.com',
+      changeOrigin: true,
+    })
+  );
+};
